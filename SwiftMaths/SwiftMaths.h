95ab36a0def2f877380e1b4fec43748d5a569c0e
Matias Eyzaguirre <matias@fortlogic.net>
2017-09-11 17:06:18 -0600

    Initial Commit

---
+++ SwiftMaths/SwiftMaths.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMaths.h
+//  SwiftMaths
+//
+//  Created by Matias Eyzaguirre on 9/11/17.
+//  Copyright © 2017 Matias Eyzaguirre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftMaths.
+FOUNDATION_EXPORT double SwiftMathsVersionNumber;
+
+//! Project version string for SwiftMaths.
+FOUNDATION_EXPORT const unsigned char SwiftMathsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMaths/PublicHeader.h>
+
+
