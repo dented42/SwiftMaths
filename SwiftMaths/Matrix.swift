fed72d2453c8de7c5bd7d3c948d926a8de82e48f
Matias Eyzaguirre <matias@fortlogic.net>
2017-09-17 19:54:21 -0600

    last commit but better.

--- SwiftMaths/Matrix.swift
+++ SwiftMaths/Matrix.swift
@@ -10,6 +10,8 @@ import Foundation
 
 public protocol Matrix {
   
+  init?(rows: Int, columns: Int)
+  
   var rowCount: Int { get }
   var columnCount: Int { get }
   
@@ -19,8 +21,6 @@ public protocol Matrix {
   var columns: CountableRange<Int> { get }
   
   subscript(r: Int, c: Int) -> Float? { get set }
-  
-  func transpose() -> Self //{ get }
   
   func row(_: Int) -> Self?
   func column(_: Int) -> Self?
@@ -32,11 +32,172 @@ public protocol Matrix {
   func subMatrix(columns: IndexSet) -> Self?
   func subMatrix(rows: IndexSet, columns: IndexSet) -> Self?
   
+  func transpose() -> Self
+  
   static func *(lhs: Float, rhs: Self) -> Self
   static func *(lhs: Self, rhs: Float) -> Self
   
   static func +(lhs: Self, rhs: Self) -> Self?
   static func -(lhs: Self, rhs: Self) -> Self?
   static func *(lhs: Self, rhs: Self) -> Self?
+  
+}
+
+// MARK: Extensions
+
+public extension Matrix {
+  
+  public init?(trace: [Float]) {
+    return nil
+  }
+  
+  public static func identity(size: Int) -> Self? {
+    return nil
+  }
+  
+}
+
+// MARK: Default Implementations
+
+public extension Matrix {
+  
+  public init?(row: [Float]) {
+    // dimensions must be valid
+    guard row.count > 0 else {
+      return nil
+    }
+    
+    self.init(rows: 1, columns: row.count)
+    
+    for idx in row.indices {
+      self[0, idx] = row[idx]
+    }
+  }
+  
+  public init?(column: [Float]) {
+    // dimensions must be valid
+    guard column.count > 0 else {
+      return nil
+    }
+    
+    self.init(rows: column.count, columns: 1)
+    
+    for idx in column.indices {
+      self[idx, 0] = column[idx]
+    }
+  }
+  
+  public var count: Int {
+    return rowCount * columnCount
+  }
+  
+  public var rows: CountableRange<Int> {
+    return 0..<rowCount
+  }
+  
+  public var columns: CountableRange<Int> {
+    return 0..<columnCount
+  }
+  
+  public func row(_ row: Int) -> Self? {
+    guard rows.contains(row) else {
+      return nil
+    }
+    
+    var mat = Self(rows: 1, columns: columnCount)!
+    
+    for column in columns {
+      mat[0, column] = self[row, column]!
+    }
+    
+    return mat
+  }
+  
+  public func column(_ column: Int) -> Self? {
+    guard columns.contains(column) else {
+      return nil
+    }
+    
+    var mat = Self(rows: rowCount, columns: 1)!
+    
+    for row in rows {
+      mat[row,0] = self[row, column]!
+    }
+    
+    return mat
+  }
+  
+  public func array(fromRow row: Int) -> [Float]? {
+    // the row needs to exist
+    guard rows.contains(row) else {
+      return nil
+    }
+    
+    return columns.map {
+      (column: Int) in
+      return self[row, column]!
+    }
+  }
+  
+  public func array(fromColumn column: Int) -> [Float]? {
+    // the column needs to exist
+    guard columns.contains(column) else {
+      return nil
+    }
+    
+    return rows.map {
+      (row: Int) in
+      return self[row, column]!
+    }
+  }
+  
+  public func subMatrix(rows: IndexSet) -> SimpleMatrix? {
+    return nil
+  }
+  
+  public func subMatrix(columns: IndexSet) -> SimpleMatrix? {
+    return nil
+  }
+  
+  public func subMatrix(rows: IndexSet, columns: IndexSet) -> SimpleMatrix? {
+    return nil
+  }
+  
+  public func transpose() -> Self {
+    var trans = Self(rows: columnCount, columns: rowCount)!
+    
+    for row in rows {
+      for column in columns {
+        trans[row, column] = self[column, row]
+      }
+    }
+    
+    return trans
+  }
+  
+  public static func *(lhs: Float, rhs: Self) -> Self {
+    return rhs
+  }
+  
+  public static func *(lhs: Self, rhs: Float) -> Self {
+    return lhs
+  }
+  
+  public static func +(lhs: Self, rhs: Self) -> Self? {
+    guard (lhs.rowCount == rhs.rowCount) && (lhs.columnCount == rhs.columnCount) else {
+      return nil
+    }
+    
+    return nil
+    
+  }
+  
+  public static func -(lhs: Self, rhs: Self) -> Self? {
+    return nil
+  }
+  
+  public static func *(lhs: Self, rhs: Self) -> Self? {
+    return nil
+  }
   
 }
---
+++ SwiftMathsTests/Tests/MatrixTests.swift
@@ -0,0 +1,396 @@
+//
+//  MatrixTests.swift
+//  SwiftMathsTests
+//
+//  Created by Matias Eyzaguirre on 9/17/17.
+//  Copyright © 2017 Matias Eyzaguirre. All rights reserved.
+//
+
+import SwiftCheck
+import XCTest
+
+import SwiftMaths
+
+class MatrixTests: XCTestCase {
+  
+  // Mark: Default methods
+  
+  func testInit_rowVector() {
+    property("empty vector is rejected") <- {
+      return SimpleMatrix(row: []) == nil
+    }
+    
+    property("dimensions are correct") <- forAll {
+      (rowHolder: ArrayOf<Float>) in
+      let row = rowHolder.getArray
+      return (row.count > 0) ==> {
+        let m = SimpleMatrix(row: row)
+        
+        let exists = (m != nil) <?> "matrix exists"
+        let rowsMatch = (m?.rowCount == 1) <?> "correct number of rows"
+        let columnsMatch = (m?.columnCount == row.count) <?> "correct number of columns"
+        
+        return exists ^&&^ rowsMatch ^&&^ columnsMatch
+      }
+    }
+    
+    property("entries are correct") <- forAll {
+      (rowHolder: ArrayOf<Float>) in
+      let row = rowHolder.getArray
+      return (row.count > 0) ==> {
+        guard let m = SimpleMatrix(row: row) else {
+          return false
+        }
+        
+        return row.indices.mapAnd {
+          (idx: Int) in
+          return m[0, idx] == row[idx]
+        }
+      }
+    }
+  }
+  
+  func testInit_columnVector() {
+    property("empty vector is rejected") <- {
+      return SimpleMatrix(column: []) == nil
+    }
+    
+    property("dimensions are correct") <- forAll {
+      (columnHolder: ArrayOf<Float>) in
+      let column = columnHolder.getArray
+      return (column.count > 0) ==> {
+        let m = SimpleMatrix(column: column)
+        
+        let exists = (m != nil) <?> "matrix exists"
+        let rowsMatch = (m?.rowCount == column.count) <?> "correct number of rows"
+        let columnsMatch = (m?.columnCount == 1) <?> "correct number of columns"
+        
+        return exists ^&&^ rowsMatch ^&&^ columnsMatch
+      }
+    }
+    
+    property("entries are correct") <- forAll {
+      (columnHolder: ArrayOf<Float>) in
+      let column = columnHolder.getArray
+      return (column.count > 0) ==> {
+        guard let m = SimpleMatrix(column: column) else {
+          return false
+        }
+        
+        return column.indices.mapAnd {
+          (idx: Int) in
+          return m[idx, 0] == column[idx]
+        }
+      }
+    }
+  }
+  
+  func testCount() {
+    property("count is correct") <- forAll {
+      (mat: SimpleMatrix) in
+      
+      return mat.count == (mat.rowCount * mat.columnCount)
+    }
+  }
+  
+  func testRows() {
+    property("correct lower bound") <- forAll {
+      (mat: SimpleMatrix) in
+      return mat.rows.first == 0
+    }
+    
+    property("correct upper bound") <- forAll {
+      (mat: SimpleMatrix) in
+      return mat.rows.last == (mat.rowCount - 1)
+    }
+  }
+  
+  func testColumns() {
+    property("correct lower bound") <- forAll {
+      (mat: SimpleMatrix) in
+      return mat.columns.first == 0
+    }
+    
+    property("correct upper bound") <- forAll {
+      (mat: SimpleMatrix) in
+      return mat.columns.last == (mat.columnCount - 1)
+    }
+  }
+
+  
+  func testRow() {
+    property("invalid rows are rejected") <- forAll {
+      (mat: SimpleMatrix, rowIdx: Int) in
+      return !mat.rows.contains(rowIdx) ==> {
+        return mat.row(rowIdx) == nil
+      }
+    }
+    
+    property("dimensions") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.rowIdxGen) {
+        (rowIdx: Int) in
+        guard let row = mat.row(rowIdx) else {
+          return false
+        }
+        
+        let rowsMatch = (row.rowCount == 1) <?> "rows"
+        let columnsMatch = (row.columnCount == mat.columnCount) <?> "columns"
+        
+        return rowsMatch ^&&^ columnsMatch
+      }
+    }
+    
+    property("contents") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.rowIdxGen) {
+        (rowIdx: Int) in
+        guard let row = mat.row(rowIdx) else {
+          return false
+        }
+        
+        return row.columns.mapAnd {
+          (idx) in
+          return row[0, idx] == mat[rowIdx, idx]
+        }
+      }
+    }
+  }
+  
+  func testColumn() {
+    property("invalid columns are rejected") <- forAll {
+      (mat: SimpleMatrix, columnIdx: Int) in
+      return !mat.columns.contains(columnIdx) ==> {
+        return mat.column(columnIdx) == nil
+      }
+    }
+    
+    property("dimensions") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.columnIdxGen) {
+        (columnIdx: Int) in
+        guard let column = mat.column(columnIdx) else {
+          return false
+        }
+        
+        let rowsMatch = (column.rowCount == mat.rowCount) <?> "rows"
+        let columnsMatch = (column.columnCount == 1) <?> "columns"
+        
+        return rowsMatch ^&&^ columnsMatch
+      }
+    }
+    
+    property("contents") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.columnIdxGen) {
+        (columnIdx: Int) in
+        guard let column = mat.column(columnIdx) else {
+          return false
+        }
+        
+        return column.rows.mapAnd {
+          (idx) in
+          return column[idx, 0] == mat[idx, columnIdx]
+        }
+      }
+    }
+  }
+  
+  func testArrayFromRow() {
+    property("invalid rows are rejected") <- forAll {
+      (mat: SimpleMatrix, rowIdx: Int) in
+      return !mat.rows.contains(rowIdx) ==> {
+        return mat.array(fromRow: rowIdx) == nil
+      }
+    }
+    
+    property("dimensions") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.rowIdxGen) {
+        (rowIdx: Int) in
+        guard let row = mat.array(fromRow: rowIdx) else {
+          return false
+        }
+        
+        return row.count == mat.columnCount
+      }
+    }
+    
+    property("contents") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.rowIdxGen) {
+        (rowIdx: Int) in
+        guard let row = mat.array(fromRow: rowIdx) else {
+          return false
+        }
+        
+        return row.indices.mapAnd {
+          (idx) in
+          return row[idx] == mat[rowIdx, idx]
+        }
+      }
+    }
+  }
+  
+  func testArrayFromColumn() {
+    property("invalid columns are rejected") <- forAll {
+      (mat: SimpleMatrix, columnIdx: Int) in
+      return !mat.columns.contains(columnIdx) ==> {
+        return mat.array(fromColumn: columnIdx) == nil
+      }
+    }
+    
+    property("dimensions") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.columnIdxGen) {
+        (columnIdx: Int) in
+        guard let column = mat.array(fromColumn: columnIdx) else {
+          return false
+        }
+        
+        return column.count == mat.rowCount
+      }
+    }
+    
+    property("contents") <- forAll {
+      (mat: SimpleMatrix) in
+      return forAll(mat.columnIdxGen) {
+        (columnIdx: Int) in
+        guard let column = mat.array(fromColumn: columnIdx) else {
+          return false
+        }
+        
+        return column.indices.mapAnd {
+          (idx) in
+          return column[idx] == mat[idx, columnIdx]
+        }
+      }
+    }
+  }
+  
+  func testSubMatrixRows() {
+    property("write tests") <- false
+  }
+  
+  func testSubMatrixColumns() {
+    property("write tests") <- false
+  }
+  
+  func testSubMatrixRowsColumns() {
+    property("write tests") <- false
+  }
+  
+  func testTranspose() {
+    property("dimensions") <- forAll {
+      (mat: SimpleMatrix) in
+      
+      let trans = mat.transpose()
+      
+      let rowsMatch = (trans.rowCount == mat.columnCount) <?> "rows"
+      let columnsMatch = (trans.columnCount == mat.rowCount) <?> "columns"
+      
+      return rowsMatch ^&&^ columnsMatch
+    }
+    
+    property("entries") <- forAll {
+      (mat: SimpleMatrix) in
+      
+      let trans = mat.transpose()
+      
+      return trans.rows.mapAnd {
+        (row) in
+        return trans.columns.mapAnd {
+          (column) in
+          return trans[row, column] == mat[column, row]
+        }
+      }
+    }
+  }
+  
+  func testScalarMultiply() {
+    property("dimensions") <- false
+    
+    property("entries") <- false
+  }
+  
+  func testMultiplyScalar() {
+    property("dimensions") <- false
+    
+    property("entries") <- false
+  }
+  
+  func testAdd() {
+    property("invalid dimensions fail") <- forAll {
+      (mat1: SimpleMatrix, mat2: SimpleMatrix) in
+      return ((mat1.rowCount != mat2.rowCount) && (mat1.columnCount != mat2.columnCount)) ==> {
+        return (mat1 + mat2) == nil
+      }
+    }
+    
+    property("dimensions") <- forAll {
+      (mat1: SimpleMatrix, mat2: SimpleMatrix) in
+      return ((mat1.rowCount == mat2.rowCount) && (mat1.columnCount == mat2.columnCount)) ==> {
+        guard let mat3 = (mat1 + mat2) else {
+          return false
+        }
+        
+        let rowsMatch = (mat3.rowCount == mat1.rowCount) <?> "rows"
+        let columnsMatch = (mat3.columnCount == mat1.columnCount) <?> "columns"
+        
+        return rowsMatch ^&&^ columnsMatch
+      }
+    }
+    
+    property("entries") <- forAll {
+      (mat1: SimpleMatrix, mat2: SimpleMatrix) in
+      return ((mat1.rowCount == mat2.rowCount) && (mat1.columnCount == mat2.columnCount)) ==> {
+        guard let mat3 = (mat1 + mat2) else {
+          return false
+        }
+        
+        return mat3.rows.mapAnd {
+          (row) in
+          return mat3.columns.mapAnd {
+            (column) in
+            return mat3[row,column] == (mat1[row,column]! + mat2[row,column]!)
+          }
+        }
+      }
+    }
+  }
+  
+  func testSubtract() {
+    property("invalid dimensions fail") <- false
+    
+    property("dimensions") <- false
+    
+    property("entries") <- false
+  }
+  
+  func testMultiply() {
+    property("invalid dimensions fail") <- false
+    
+    property("dimensions") <- false
+    
+    property("entries") <- false
+  }
+  
+  // Mark: Additional methods
+  
+  func testInit_trace() {
+    property("invalid dimensions fail") <- false
+    
+    property("dimensions") <- false
+    
+    property("entries") <- false
+  }
+  
+  func testIdentity() {
+    property("invalid dimensions fail") <- false
+    
+    property("dimensions") <- false
+    
+    property("entries") <- false
+  }
+
+}
